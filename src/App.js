dc4c065cd25ed89c0becdcc40527d56f3758bc74
Dilina Madhushan <dilina@xdoto.io>
2022-05-22 17:44:26 +0530

    appbar color error fixed, theme changing complete

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import { Routes, Route, BrowserRouter } from "react-router-dom"
 import Layout from "./components/Layout"
 import ROUTES from "./routes/config"
 import Login from "./pages/Login"
+import { useEffect, useState } from "react"
 
 function App() {
+
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        const localTheme = localStorage.getItem("THEME");
+        if(localTheme === 'dark'){
+            setTheme('dark');
+        }
+    });
+
     return (
-        <ThemeProvider theme={getTheme("dark")}>
+        <ThemeProvider theme={getTheme(theme)}>
             <BrowserRouter>
                 <Routes>
                     <Route path={'/login'} element={<Login />} />
                     {ROUTES.map((route, i) => (
                         <Route
                             path={route.path}
-                            element={<Layout isLayoutHide={route.hideLayout}>{route.element}</Layout>}
+                            element={<Layout theme={theme} setTheme={setTheme} isLayoutHide={route.hideLayout}>{route.element}</Layout>}
                             key={"route" + route.id}
                         />
                     ))}
